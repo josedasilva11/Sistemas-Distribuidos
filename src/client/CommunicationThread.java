db75d935154dab16fdfc8d92c73d64e0855135c1
Marjers <js.pdo.silv@gmail.com>
2023-12-13 16:15:42 +0000

    Added stuff

---
+++ src/client/CommunicationThread.java
@@ -0,0 +1,61 @@
+package src.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class CommunicationThread implements Runnable {
+    private final Socket socket;
+    private final DataOutputStream out;
+    private final DataInputStream in;
+    private final ConcurrentLinkedQueue<Request> requestQueue;
+    private final ResponseCallback responseCallback; // Adicionando o callback
+
+    public CommunicationThread(Socket socket, ResponseCallback responseCallback) throws IOException {
+        this.socket = socket;
+        this.out = new DataOutputStream(socket.getOutputStream());
+        this.in = new DataInputStream(socket.getInputStream());
+        this.requestQueue = new ConcurrentLinkedQueue<>();
+        this.responseCallback = responseCallback; // Inicializando o callback
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                sendPendingRequests();
+                listenForResponses();
+            }
+        } catch (IOException e) {
+            // Handle exceptions
+        }
+    }
+
+    private void sendPendingRequests() throws IOException {
+        while (!requestQueue.isEmpty()) {
+            Request request = requestQueue.poll();
+            // Send request to server
+            out.writeUTF(request.getAction());
+            // ... Other data as required
+        }
+    }
+
+    private void listenForResponses() {
+        new Thread(() -> {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    String response = in.readUTF();
+                    responseCallback.onResponseReceived(response);
+                }
+            } catch (IOException e) {
+                // Logar ou lidar com a exceção
+            }
+        }).start();
+    }
+
+    public void addRequest(Request request) {
+        requestQueue.add(request);
+    }
+
+    // Define a Request class or use a suitable data structure for your requests
+}
--- src/client/EchoClient.java
+++ src/client/EchoClient.java
@@ -6,18 +6,14 @@ import java.util.Scanner;
 
 public class EchoClient {
     public static void main(String[] args) {
-        // Definição do endereço do servidor e porta
         String hostName = "localhost";
         int port = 1234;
-        // Inicia o scanner para leitura de entrada do utilizador
         Scanner scanner = new Scanner(System.in);
 
-        // Tentativa de estabelecer conexão com o servidor
         try (Socket socket = new Socket(hostName, port);
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 DataInputStream in = new DataInputStream(socket.getInputStream())) {
 
-            // Solicita ao utilizador para escolher entre registo ou login
             System.out.println("Digite 'register' para se registar ou 'login' para entrar:");
             String action = scanner.nextLine();
 
@@ -50,42 +46,42 @@ public class EchoClient {
 
             // Autenticação bem-sucedida
             if (response.startsWith("Bem-vindo")) {
-                // Oferece ao utilizador a escolha da ação
                 System.out.println(
                         "Digite 'enviar' para enviar uma tarefa ou 'status' para consultar o estado do serviço:");
                 String nextAction = scanner.nextLine();
 
-                // Lógica para enviar uma tarefa
                 if ("enviar".equals(nextAction)) {
+                    System.out.println("Escreva o caminho do arquivo de tarefa:");
+                    String filePath = scanner.nextLine();
+                    File file = new File(filePath);
+                    byte[] fileContent = new byte[(int) file.length()];
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        fis.read(fileContent);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Arquivo não encontrado: " + e.getMessage());
+                        return;
+                    } catch (IOException e) {
+                        System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+                        return;
+                    }
+
                     System.out.println("Escreva a quantidade de memória necessária para a tarefa (em MB):");
                     long memoryRequired = Long.parseLong(scanner.nextLine());
-                    // Envia dados da tarefa para o servidor
+
                     out.writeLong(memoryRequired);
-                    out.writeUTF("dados da tarefa");
-                    // Recebe e exibe a resposta do servidor sobre a tarefa
+                    out.writeInt(fileContent.length); // Envia o tamanho do arquivo
+                    out.write(fileContent); // Envia o conteúdo do arquivo
+
                     String taskResponse = in.readUTF();
                     System.out.println("Resposta do servidor: " + taskResponse);
-                }
-                // Lógica para consultar o estado do serviço
-                else if ("status".equals(nextAction)) {
-                    // Envia o comando de consulta para o servidor
-                    out.writeUTF("status");
-                    // Recebe e exibe informações de memória e tarefas pendentes
-                    String memoryStatus = in.readUTF();
-                    String taskStatus = in.readUTF();
-                    System.out.println(memoryStatus);
-                    System.out.println(taskStatus);
+                } else if ("status".equals(nextAction)) {
+                    // [Código para consulta de status]
                 }
             }
 
-        }
-        // Tratamento de exceções relacionadas à rede
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Erro ao conectar com o servidor: " + e.getMessage());
-        }
-        // Bloco finally para garantir que o scanner é fechado
-        finally {
-            // Fechar o scanner para evitar libertação de recursos
+        } finally {
             if (scanner != null) {
                 scanner.close();
             }
